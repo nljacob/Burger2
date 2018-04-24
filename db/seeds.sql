2bfa14f408d0dec5094c6012a48846a5545a19a9
nljacob <neehaljacob@gmail.com>
2018-04-23 19:33:01 -0700

    completed hw

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Yummy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Yummy Yummy Burger');
+INSERT INTO burgers (burger_name) VALUES ('Super Yummy Burger');
